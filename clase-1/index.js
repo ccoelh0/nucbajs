2566cb6cec36fa94dca01130a990fdacae684261
aguscoelho <agustin@conexa.ai>
2023-11-01 00:26:07 -0300

    start

---
+++ clase-1/index.js
@@ -0,0 +1,38 @@
+// Variable y tipos de datos
+
+var hello = 'hola mundo';
+var someNumber = 1;
+var someBoolean = false;
+var someUndefined = undefined
+
+// console.log('String -> ', hello);
+// console.log('Number -> ', someNumber);
+// console.log('Boolean -> ', someBoolean);
+// console.log('someUndefined -> ', someUndefined);
+
+// Suma y resta
+// console.log('Una resta ->', someNumber - someNumber);
+// Concatenacion
+// console.log('Concatenacion ->', hello + ' ' + someNumber);
+
+// Reasignacion
+var saludo = 'Hola Fran!';
+console.log('Saludo a franco ->', saludo);
+var saludo = 'Hola Lauti!!';
+console.log('Saludo a ??? ->', saludo);
+
+let suma = 1;
+console.log('total ->', suma);
+suma = suma + 1;
+console.log('nuevo total ->', suma);
+
+const sumaConst = 1; 
+console.log('suma const -> ', sumaConst)
+// sumaConst = 10;
+// console.log('suma const redeclarado -> ', sumaConst)
+
+// prompt
+
+var userName = 'Sabrina!';
+// console.log('nombre del usuario ->', userName);
+var alert = alert(`Hola ${userName}`);
